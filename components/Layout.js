fe36814c342559e00a4b98c4dcf12f85841e89fa
Gino Pietrobon <gino.b.pietrobon@gmail.com>
2021-04-17 16:20:14 -0300

    fixed contact section

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { AiOutlineBell, AiOutlineUser } from "react-icons/ai";
 import { GiMonkey } from "react-icons/gi";
-import { FiSearch } from "react-icons/fi";
-import { GiHamburgerMenu } from "react-icons/gi"
+import { FiSearch, FiMail } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaWhatsapp, FaInstagram, FaFacebook, FaLinkedinIn, FaGithub  } from "react-icons/fa"
 import Link from "next/link";
 
 
 export default function Layout({ children }) {
-  
   const [open, setOpen] = useState(false);
 
   const toggleNav = (e) => {
@@ -42,45 +42,47 @@ export default function Layout({ children }) {
             <GiHamburgerMenu />
           </button>
         </div>
-        
       </div>
-      <div className="absolute z-20 bg-black text-white w-full" style={{ display: open ? 'block' : 'none' }}>
-        <div className="flex flex-col text-base p-4 text-mywhite divide-y" >
-        <Link href="/">
-          <a className="mr-10  mt-5 px-5 border-b-2 border-transparent hover:border-black">
-            Home
-          </a>
-        </Link>
-        <Link href="/article">
-          <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
-            Blog
-          </a>
-        </Link>
-        <Link href="/news">
-          <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
-            News
-          </a>
-        </Link>
-        <Link href="/dev">
-          <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
-            Dev
-          </a>
-        </Link>
-        <Link href="/cripto">
-          <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
-            Cripto
-          </a>
-        </Link>
-        <Link href="/politica">
-          <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
-            Política
-          </a>
-        </Link>
-        <Link href="/escritos">
-          <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
-            Escritos
-          </a>
-        </Link>
+      <div
+        className="absolute z-20 bg-black text-white w-full"
+        style={{ display: open ? "block" : "none" }}
+      >
+        <div className="flex flex-col text-base p-4 text-mywhite divide-y">
+          <Link href="/">
+            <a className="mr-10  mt-5 px-5 border-b-2 border-transparent hover:border-black">
+              Home
+            </a>
+          </Link>
+          <Link href="/article">
+            <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
+              Blog
+            </a>
+          </Link>
+          <Link href="/news">
+            <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
+              News
+            </a>
+          </Link>
+          <Link href="/dev">
+            <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
+              Dev
+            </a>
+          </Link>
+          <Link href="/cripto">
+            <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
+              Cripto
+            </a>
+          </Link>
+          <Link href="/politica">
+            <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
+              Política
+            </a>
+          </Link>
+          <Link href="/escritos">
+            <a className="mr-10 mt-5 px-5 border-b-2 border-transparent hover:border-black">
+              Escritos
+            </a>
+          </Link>
         </div>
       </div>
 
@@ -127,48 +129,50 @@ export default function Layout({ children }) {
       <div className="bg-black text-white text-xl py-10 px-6 md:px-24 flex flex-col">
         <div>
           <Link href="/">
-            <a className="text-white text-2xl flex align-baseline px-10 my-auto">
-              <GiMonkey className="inline text-4xl mr-4" /> moonwatcher.dev
+            <a className="text-white text-lg md:text-2xl flex align-baseline px-10 my-auto hover:text-blue-700">
+              <GiMonkey className="inline text-4xl mr-4 " /> moonwatcher.dev
             </a>
           </Link>
         </div>
 
-        <div className="grid text-center grid-cols-3 grid-rows-3 gap-2 mt-10 max-w-3xl  text-base">
-              <Link href="/contacto">
-                <span className=" text-green-600 cursor-pointer font-bold">
-                ♦ Whatsapp ♦
-                </span>
-              </Link>
-              
-              <Link href="/contacto">
-                <span className=" text-purple-600 cursor-pointer font-bold">
-                ♦ Instagram ♦
-                 </span>
-              </Link>
-              
-              <Link href="/contacto">
-                <span className=" text-blue-800 cursor-pointer font-bold">
-                 ♦ Facebook ♦
-                </span>
-              </Link>
-              
-              <Link href="/contacto">
-                <span className=" text-blue-900 cursor-pointer font-bold">
-                  ♦ LinkedIn ♦
-                </span>
-              </Link>
-              
-              <Link href="/contacto">
-                <span className=" text-gray-700 cursor-pointer font-bold">
-                  ♦ Github ♦
-                </span>
-              </Link>
+        <div className="flex space-x-5 md:space-x-10 mt-10 max-w-3xl text-4xl">
+          
+          <Link href="https://api.whatsapp.com/send?phone=5492616708100" target="_blank">
+            
+          <a target="_blank">
+            <FaWhatsapp className="hover:text-green-600" />
+            </a>
+          </Link>
 
-              <Link href="/contacto">
-                <span className=" text-red-700 cursor-pointer font-bold">
-               ♦ Gmail ♦
-                </span>
-              </Link>
+          <Link href="https://www.instagram.com/moonwatcher.dev/" >
+            <a target="_blank">
+              <FaInstagram className="hover:text-indigo-500" />
+            </a>
+          </Link>
+
+          <Link href="https://www.facebook.com/Gini77o">
+          <a target="_blank"> 
+            <FaFacebook className="hover:text-blue-600" />
+            </a>
+          </Link>
+
+          <Link href="https://www.linkedin.com/in/gino-pietrobon">
+          <a target="_blank">
+            <FaLinkedinIn className="hover:text-yellow-400" />
+            </a>
+          </Link>
+
+          <Link href="https://github.com/DevZeppelin">
+          <a target="_blank"> 
+            <FaGithub className="hover:text-red-400" />
+            </a>
+          </Link>
+
+          <Link href="mailto:gino.b.pietrobon@gmail.com">
+           <a target="_blank"> 
+            <FiMail className="hover:text-red-700" />
+            </a>
+          </Link>
         </div>
 
         <div className="mt-10 text-sm max-w-2xl">
@@ -176,7 +180,6 @@ export default function Layout({ children }) {
           {new Date().getFullYear()}
         </div>
       </div>
-      
     </div>
   );
 }
